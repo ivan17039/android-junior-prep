a51fa59e9cfd46dca8269cfef46ef34e1d9f9aef
ivan17039 <ivanbarada1703@gmail.com>
2026-07-23 22:53:29 +0200

    Day 4 complete: Implement state with remember, mutableStateOf and LazyColumn

---
+++ app/src/main/java/com/ivanb/jobprepapp/BookList.kt
@@ -0,0 +1,61 @@
+package com.ivanb.jobprepapp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BookScreen(books: List<Book>, modifier: Modifier = Modifier) {
+    var count by remember {mutableStateOf(0)  }
+    LazyColumn {
+
+        item {
+            Row{
+                Button(onClick = {count++ }){
+                    Text("Kliknuto $count puta")
+
+                }
+                Button(onClick = {count=0}){
+                    Text("Reset")
+                }
+            }
+        }
+
+        items(books) { book ->
+            BookCard(book = book)
+        }
+
+        item {
+            Text(
+                text = "Lista brojeva:",
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+        items((1..50).toList()) { number ->
+            Text(
+                text = "Broj: $number",
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, heightDp = 1200)
+@Composable
+fun BookScreenPreview() {
+    BookScreen(books = sampleBooks)
+}
--- app/src/main/java/com/ivanb/jobprepapp/MainActivity.kt
+++ app/src/main/java/com/ivanb/jobprepapp/MainActivity.kt
@@ -5,31 +5,37 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ivanb.jobprepapp.ui.theme.JobPrepAppTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Box
-import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,63 +43,56 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MojEkran("Ivan")
-
         }
     }
 }
 
 @Composable
 fun MojEkran(name: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier){
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+        Text(text = "Hello $name!")
+
         ExampleColumn()
         ExampleRow()
         ExampleBox()
 
-        Text(
-            text = "Pozdrav",
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
-                .background(Color.LightGray)
-        )
-        // Bitan je redoslijed
-        Text(
-            text = "Pozdrav",
-            modifier = Modifier
-                .background(Color.LightGray)
-                .padding(16.dp)
-                .fillMaxWidth()
 
+        Text(
+            text = "Pozdrav",
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .background(Color.LightGray)
         )
-        // 1. Slika (koristi zadanu Android ikonu)
+        Text(
+            text = "Pozdrav",
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
             contentDescription = "Profilna slika"
         )
-
-        // 2. Tekst
         Text(
             text = "Junior Android Developer",
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             color = Color.DarkGray
         )
-
-        // 3. Gumb
         Button(
-            onClick = { /* Ovdje pišemo logiku kad netko klikne */ },
+            onClick = { /* Klik logika */ },
             modifier = Modifier.padding(top = 12.dp)
         ) {
             Text("Klikni me")
         }
-        BookCard(book = sampleBooks[0])
+
+        // AKO IMAS BookCard I sampleBooks OTKOMENTIRAJ OVO:
+        // BookCard(book = sampleBooks[0])
 
         Text(
-            text="Zadaci za 3. dan!!!",
+            text = "Zadaci za 3. dan!!!",
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             color = Color.DarkGray
@@ -101,16 +100,17 @@ fun MojEkran(name: String, modifier: Modifier = Modifier) {
         PersonCard("Ivan", 23)
         RowArrangment()
         BoxElement()
+        SimpleCounter()
+        Counter()
     }
-
 }
 
 @Composable
 fun ExampleColumn() {
     Column {
-        Text( text = "Prvi red")
-        Text( text = "Drugi red")
-        Text( text = "Treći red")
+        Text(text = "Prvi red")
+        Text(text = "Drugi red")
+        Text(text = "Treći red")
     }
 }
 
@@ -127,27 +127,29 @@ fun ExampleRow() {
 fun ExampleBox() {
     Box {
         Text("Ispod")
-        Text("Iznad")   // renderira se preko prethodnog
+        Text("Iznad")
     }
 }
+
 @Composable
-fun PersonCard(name: String, age: Int){
+fun PersonCard(name: String, age: Int) {
     Column(
         modifier = Modifier
             .padding(all = 16.dp)
             .clip(RoundedCornerShape(12.dp))
             .background(Color.LightGray)
-    ){
+    ) {
         Text("Ime: $name")
         Text("Godine: $age")
     }
 }
+
 @Composable
-fun RowArrangment(){
+fun RowArrangment() {
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween
-    ){
+    ) {
         Box(
             modifier = Modifier
                 .size(10.dp)
@@ -155,15 +157,43 @@ fun RowArrangment(){
         )
         Text(text = "Neki tekst")
     }
-
 }
+
 @Composable
-fun BoxElement(){
-    Box (
-        modifier = Modifier.size(100.dp) .background(Color.Red)
+fun BoxElement() {
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(Color.Red)
     ) {
         Text(text = "Preko", modifier = Modifier.align(Alignment.Center))
     }
+}
+
+@Composable
+fun Counter() {
+    // Korištenje mutableIntStateOf + 'by' delegacije
+    var count by remember { mutableStateOf(0) }
+
+    Button(onClick = { count++ }) {
+        Text("Kliknuto $count puta")
+    }
+}
+
+@Composable
+fun SimpleCounter(){
+    var count by remember {mutableStateOf(0)  }
+    Row{
+        Button(onClick = {count++ }){
+            Text("Kliknuto $count puta")
+
+        }
+        Button(onClick = {count=0}){
+            Text("Reset")
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
